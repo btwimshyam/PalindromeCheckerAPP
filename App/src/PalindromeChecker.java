94e423932fead5ecb1d9d2127c95585d3288429d
Priyan66 <priyanshuvs66@gmail.com>
2026-02-17 10:50:56 +0530

    Case Insensitive

--- App/src/PalindromeChecker.java
+++ App/src/PalindromeChecker.java
@@ -1,15 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-static boolean check(String s, int start, int end){
-    if(start >= end) return true;
-    if(s.charAt(start) != s.charAt(end)) return false;
-    return check(s, start+1, end-1);
-}
-void main() {
-    String input = "madam";
 
-    boolean result = check(input, 0, input.length() - 1);
+void main() {
+    String input = "A man a plan a canal Panama";
+
+    String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
+
+    boolean isPalindrome = true;
+
+    for(int i=0;i<normalized.length()/2;i++){
+        if(normalized.charAt(i) !=
+                normalized.charAt(normalized.length()-1-i)){
+            isPalindrome = false;
+            break;
+        }
+    }
 
     System.out.println("Input : " + input);
-    System.out.println("Is Palindrome? : " + result);
+    System.out.println("Is Palindrome? : " + isPalindrome);
 }
